ab90ff227d0dac880aa225a6612c6b9cfcc9e3a6
Oshadha Thambavita <osthambe20@gmail.com>
2026-03-18 10:35:54 +0530

    initialize the project structure

---
+++ sensor-telemetry-service/src/main/java/edu/lk/ijse/gdse/sensor_telemetry_service/Application.java
@@ -0,0 +1,13 @@
+package edu.lk.ijse.gdse.sensor_telemetry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ sensor-telemetry-service/src/test/java/edu/lk/ijse/gdse/sensor_telemetry_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lk.ijse.gdse.sensor_telemetry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
