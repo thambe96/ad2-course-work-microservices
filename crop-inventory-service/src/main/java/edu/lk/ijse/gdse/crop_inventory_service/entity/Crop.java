65fd8032c27c94db9e5bfab6f0813870984a9c24
Oshadha Thambavita <osthambe20@gmail.com>
2026-04-11 00:21:03 +0530

    Implement Crop entity

--- crop-inventory-service/src/main/java/edu/lk/ijse/gdse/crop_inventory_service/entity/Crop.java
+++ crop-inventory-service/src/main/java/edu/lk/ijse/gdse/crop_inventory_service/entity/Crop.java
@@ -1,17 +1,26 @@
 package edu.lk.ijse.gdse.crop_inventory_service.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
+@Entity
 public class Crop {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String batchId;
+    private String cropName;
 
+    @Enumerated(EnumType.STRING)
+    private StateMachine stateMachine;
+    private Instant registeredAT;
 
 
 }
