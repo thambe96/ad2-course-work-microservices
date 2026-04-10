a077e93d82f50804ccbbc69b6ccd66b6476d6725
Oshadha Thambavita <osthambe20@gmail.com>
2026-04-10 09:34:54 +0530

    Add a method called findZoneTempsNative in ZoneRepo to get min and max temperature for a give zone based on zoneId and deviceId

--- zone-management-service/src/main/java/edu/lk/ijse/gdse/zone_management_service/repository/ZoneRepo.java
+++ zone-management-service/src/main/java/edu/lk/ijse/gdse/zone_management_service/repository/ZoneRepo.java
@@ -1,9 +1,21 @@
 package edu.lk.ijse.gdse.zone_management_service.repository;
 
+
 import edu.lk.ijse.gdse.zone_management_service.entity.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ZoneRepo extends JpaRepository<Zone, String> {
+
+    @Query(value = "SELECT device_id, max_temp, min_temp FROM zone " +
+            "WHERE zone_id = :zoneId AND device_id = :deviceId",
+            nativeQuery = true)
+    Object[] findZoneTempsNative(@Param("zoneId") String zoneId,
+                                 @Param("deviceId") String deviceId);
+
+
+
 }
