eeceaf0300610ab300d21dc767c8744a331be3b3
Oshadha Thambavita <osthambe20@gmail.com>
2026-04-10 09:51:27 +0530

    Implement getZoneThresholdLimits in service layer in zone-management-service

--- zone-management-service/src/main/java/edu/lk/ijse/gdse/zone_management_service/service/ZoneService.java
+++ zone-management-service/src/main/java/edu/lk/ijse/gdse/zone_management_service/service/ZoneService.java
@@ -1,10 +1,12 @@
 package edu.lk.ijse.gdse.zone_management_service.service;
 
 import edu.lk.ijse.gdse.zone_management_service.dto.RequestZoneDTO;
+import edu.lk.ijse.gdse.zone_management_service.dto.ThresholdLimitsDTO;
 import edu.lk.ijse.gdse.zone_management_service.dto.ZoneDTO;
 
 public interface ZoneService {
 
     ZoneDTO saveZoneWithDevice(RequestZoneDTO requestZoneDTO, String authHeader);
+    ThresholdLimitsDTO getZoneThresholdLimits(String zoneId, String deviceId);
 
 }
--- zone-management-service/src/main/java/edu/lk/ijse/gdse/zone_management_service/service/impl/ZoneServiceImpl.java
+++ zone-management-service/src/main/java/edu/lk/ijse/gdse/zone_management_service/service/impl/ZoneServiceImpl.java
@@ -1,10 +1,7 @@
 package edu.lk.ijse.gdse.zone_management_service.service.impl;
 
 import edu.lk.ijse.gdse.zone_management_service.client.IoTServerClient;
-import edu.lk.ijse.gdse.zone_management_service.dto.IotServerResponse;
-import edu.lk.ijse.gdse.zone_management_service.dto.RequestZoneDTO;
-import edu.lk.ijse.gdse.zone_management_service.dto.ZoneDTO;
-import edu.lk.ijse.gdse.zone_management_service.dto.ZoneRegDTO;
+import edu.lk.ijse.gdse.zone_management_service.dto.*;
 import edu.lk.ijse.gdse.zone_management_service.entity.Zone;
 import edu.lk.ijse.gdse.zone_management_service.repository.ZoneRepo;
 import edu.lk.ijse.gdse.zone_management_service.service.ZoneService;
@@ -48,5 +45,25 @@ public class ZoneServiceImpl implements ZoneService {
 
 
         return modelMapper.map(zoneRepo.save(zone), ZoneDTO.class);
+    }
+
+    @Override
+    public ThresholdLimitsDTO getZoneThresholdLimits(String zoneId, String deviceId) {
+
+        Object[] threshHoldLimits = zoneRepo.findZoneTempsNative(zoneId, deviceId);
+
+        if (threshHoldLimits == null) {
+            return null;
+        }
+
+        double minTemp = threshHoldLimits[1] != null ? (Double) threshHoldLimits[1] : 0.0;
+        double maxTemp = threshHoldLimits[2] != null ? (Double) threshHoldLimits[2] : 0.0;
+
+
+        return new ThresholdLimitsDTO(
+                (String) threshHoldLimits[0],
+                minTemp,
+                maxTemp
+        );
     }
 }
