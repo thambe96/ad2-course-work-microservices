0f871cfbacfd96fc3b3fc607b857a96104bcfb1d
Oshadha Thambavita <osthambe20@gmail.com>
2026-04-10 12:20:09 +0530

    Configure ZoneThresholdClient FeignClient to fetch threshold limits from the zone-management-service

--- automation-control-service/src/main/java/edu/lk/ijse/gdse/automation_control_service/client/ZoneThresholdClient.java
+++ automation-control-service/src/main/java/edu/lk/ijse/gdse/automation_control_service/client/ZoneThresholdClient.java
@@ -1,5 +1,16 @@
 package edu.lk.ijse.gdse.automation_control_service.client;
 
+import edu.lk.ijse.gdse.automation_control_service.dto.ZoneThresholdsDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@FeignClient(name = "zone-thresholds-client", url = "http://localhost:8081/api/zones")
 public interface ZoneThresholdClient {
+
+    @GetMapping(value = "/get-threshold-limits")
+    ZoneThresholdsDTO getZoneThresholds(
+            @RequestParam("zoneId") String zoneId,
+            @RequestParam("deviceId") String deviceId);
+
 }
---
+++ automation-control-service/src/main/java/edu/lk/ijse/gdse/automation_control_service/entity/AutomationLog.java
@@ -0,0 +1,4 @@
+package edu.lk.ijse.gdse.automation_control_service.entity;
+
+public class AutomationLog {
+}
